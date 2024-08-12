74b960f430f61a08458f1c8bc75996b3ff3ce44d
Jose Flores <20221043@uthh.edu.mx>
2024-08-11 20:04:16 -0600

    sks

--- src/app.js
+++ src/app.js
@@ -50,9 +50,4 @@ app.use("/api/estados", estadosRouter);
 app.use("/api/estadosOrdenTrabajo", estadosOrdenTrabajoRouter);
 app.use("/api/hisorialSolicitud", historialSoliRoutes);
 
-// Manejo de errores 404 para rutas no encontradas
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Not Found" });
-});
-
 export default app;
