fa54af22afee75c1a3608b8e07f2adcc8be481e6
Eduardoryt <20221076@uthh.edu.mx>
2024-08-30 19:40:43 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const PORT = process.env.PORT || 4000;
 
 export const MONGODB_URI =
   process.env.MONGODB_URI ||
-  "mongodb+srv://Eduardo:miProyecto123@proyectsoliyorden.lnh5t.mongodb.net/dbProyectSoliYOrden?retryWrites=true&w=majority&appName=proyectSoliYOrden";
+  "mongodb+srv://HolaMundo:qpROPyu1ySp7EwuU@proyectsoliyorden.lnh5t.mongodb.net/dbProyectSoliYOrden?retryWrites=true&w=majority&appName=proyectSoliYOrden";
 
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
